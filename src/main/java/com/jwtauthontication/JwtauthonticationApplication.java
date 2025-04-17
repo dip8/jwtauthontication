a2a78ace91fa7592d57768b761c200fb2d724879
Dipak.Lokhande <dipak.lokhande@medimaze.in>
2025-04-17 18:25:46 +0530

    initial commit

---
+++ src/main/java/com/jwtauthontication/JwtauthonticationApplication.java
@@ -0,0 +1,13 @@
+package com.jwtauthontication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtauthonticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtauthonticationApplication.class, args);
+	}
+
+}
