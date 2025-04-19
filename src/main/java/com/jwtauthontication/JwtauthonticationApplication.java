cb5cc87b1b0c7a042c594e94db5169a9296c1ef2
dip8 <dipaklokhande81@gmail.com>
2025-04-19 15:59:17 +0530

    complete code commit added

--- src/main/java/com/jwtauthontication/JwtauthonticationApplication.java
+++ src/main/java/com/jwtauthontication/JwtauthonticationApplication.java
@@ -1,12 +1,18 @@
 package com.jwtauthontication;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@SecurityScheme(name = "Bearer Auth", description = "Provide JWT Token", scheme = "Bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
 public class JwtauthonticationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JwtauthonticationApplication.class, args);
 	}
 
--- src/main/java/com/jwtauthontication/service/ProductServiceImpl.java
+++ src/main/java/com/jwtauthontication/service/ProductServiceImpl.java
@@ -5,6 +5,8 @@ import com.jwtauthontication.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductServiceImpl {
     @Autowired
