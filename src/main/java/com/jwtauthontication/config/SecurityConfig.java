2a240ec03d1d612fe5bd40cbc6bddf00e81804fe
dip8 <dipaklokhande81@gmail.com>
2025-04-20 14:11:24 +0530

    complete code commit added final touch

--- src/main/java/com/jwtauthontication/config/SecurityConfig.java
+++ src/main/java/com/jwtauthontication/config/SecurityConfig.java
@@ -64,6 +64,8 @@ public class SecurityConfig {
                                 .permitAll()
                                 .requestMatchers("/auth/**")
                                 .permitAll()
+                                .requestMatchers("/products/**")
+                                .permitAll()
                                 .anyRequest()
                                 .authenticated())
                 .exceptionHandling(exception ->
